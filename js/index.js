a7eed79eefa5fffaa273248723bd9ad875208b07
ptal0003 <ptal0003@student.monash.edu>
2023-04-30 12:20:55 +1000

    updated manifest V3

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function helloThere(){
 
 function GenerateResponse(){
     console.log(document.getElementById("user_response_prompt").value)
+    document.getElementById("email_context").value = document.getElementById("user_response_prompt").value;
 }
 
 document.getElementById("generateResponseButton").addEventListener('click',GenerateResponse)
