719dda212890155220e877ab5ef82c270d37cadc
ptal0003 <ptal0003@student.monash.edu>
2023-05-04 18:56:30 +1000

    Fixed displaying the Generate Email button

--- js/background.js
+++ js/background.js
@@ -1,8 +1,9 @@
 console.log("Background running");
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("mail.google.com")){
-        chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-        })
-    }
-})
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//     console.log("It updated")
+//     if (tab.url && tab.url.includes("mail.google.com")){
+//         chrome.tabs.sendMessage(tabId, {
+//             type: "NEW",
+//         })
+//     }
+// })
--- js/index.js
+++ js/index.js
@@ -25,9 +25,10 @@ function GenerateResponse(){
 console.log("Chrome extension")
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
     const { type } = obj;
+    var sendButton = document.querySelector('div[data-tooltip="Send (Ctrl-Enter)"]');
     let actionButtonSections = document.getElementsByClassName("btC")
     const emailAIBtnExists = document.getElementsByClassName("email-ai-writer-btn")[0];
-    if(actionButtonSections.length > 0){
+    if(sendButton){
         
         if (!emailAIBtnExists){
             let newBtn = document.createElement("btn");
@@ -53,5 +54,37 @@ chrome.runtime.onMessage.addListener((obj, sender, response) => {
     }
     
 })
-
-init();
+// init();
+var observer = new MutationObserver(showMessage);
+observer.observe(document, { childList: true, subtree: true });
+function showMessage(){
+    console.log("Change detected")
+    let actionButtonSections = document.getElementsByClassName("btC")
+    const emailAIBtn = document.getElementsByClassName("email-ai-writer-btn");
+    if(actionButtonSections.length > 0){
+        
+        if (emailAIBtn.length < actionButtonSections.length){
+            let newBtn = document.createElement("btn");
+            newBtn.className = "email-ai-writer-btn";
+            newBtn.innerText = "Generate Email"
+            newBtn.style.fontSize = "small";
+            newBtn.style.borderRadius = "5px";
+            newBtn.style.width = "100px";
+            newBtn.style.padding = "10px 10px 10px 10px";
+            newBtn.style.backgroundColor = "DarkBlue";
+            newBtn.style.color = "DarkGrey";
+            newBtn.style.textAlign = "center";
+            newBtn.style.marginLeft = "15px"
+            newBtn.addEventListener("click", function(){
+                window.alert(newBtn.id);
+            })
+            for (let i = 0; i < actionButtonSections.length; i++){
+                if(!actionButtonSections[i].getElementsByClassName("email-ai-writer-btn")[0])
+                {
+                    newBtn.id = actionButtonSections[i].id + "-" + newBtn.className
+                    actionButtonSections[i].appendChild(newBtn);
+                }               
+            } 
+        }
+    }
+}
